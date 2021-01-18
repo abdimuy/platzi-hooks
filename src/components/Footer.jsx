3ef4ebac6183de368e6bce5ce4794c899e673b6f
Aldrich Cortero <abdimuy@gmai.com>
2021-01-18 16:26:00 -0600

    [add] firebase hosting

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import '../styles/components/Footer.css';
+
+const Footer = () => {
+	return (
+		<div clasName="Footer">
+			<p className="Footer-title">PlatziConf Mech</p>
+			<p className="Footer-copy">Todos los Izquierdos reservados</p>
+		</div>
+	)
+}
+
+export default Footer;
